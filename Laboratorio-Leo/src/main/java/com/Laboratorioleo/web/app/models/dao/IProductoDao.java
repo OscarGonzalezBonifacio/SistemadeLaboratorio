32172dfe431b3ca5abe5d4e958b091ff31bd17d2
OscarGonzalezBonifacio <oscarglzatc@gmail.com>
2021-06-20 23:50:19 -0500

    actualizacion de archivos de los servicios

--- Laboratorio-Leo/src/main/java/com/Laboratorioleo/web/app/models/dao/IProductoDao.java
+++ Laboratorio-Leo/src/main/java/com/Laboratorioleo/web/app/models/dao/IProductoDao.java
@@ -3,11 +3,12 @@ package com.Laboratorioleo.web.app.models.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.Laboratorioleo.web.app.models.entity.Producto;
 
-public interface IProductoDao extends CrudRepository<Producto, Long>{
+public interface IProductoDao extends PagingAndSortingRepository<Producto, Long>{
 
 	@Query("select p from Producto p where p.nombre like %?1%")
 	public List<Producto> findByNombre(String term);
